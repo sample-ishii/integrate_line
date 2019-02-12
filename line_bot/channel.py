b5176688725f58f53c87ce5bda25d15a0553cc10
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-02-12 16:21:49 +0900

    Delete channel.py

--- line_bot/channel.py
+++
@@ -1,9 +0,0 @@
-"""
-channel ID and tokens
-"""
-
-user_id="Ue8d38351e975746041eda06ae8365fcf"
-channel_id="1644346617"
-channel_secret="93a7ffdf783dfcb5f7981816fea242e5"
-access_token="p+khqNnm59TrgG7jppvqp1ZYDVI8vE5TzrxWdvRKN4yN4sRu2s31D8HFd1h9zyI/Xkix3QJskxawUvmJ/bFH/dTUmFcO3R0caq9NuFkflU7+XTQve2ZX+pJstxxuGPnPritNQ5IFglugScAWhE38xAdB04t89/1O/w1cDnyilFU="
-webhook_url="https://connectcrew-app.herokuapp.com/callback"
