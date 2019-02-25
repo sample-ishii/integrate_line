e39573e54136681736bffe910742adaf6d762537
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-02-25 15:03:27 +0900

    Add files via upload
    コマンドの追加

--- line_bot/account_response.py
+++ line_bot/account_response.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import random
 
-dic={"会社概要":"COMPANY", "事業内容":"SERVICE", "採用情報":"CAREER", "コマンド":"COMMAND"}
+dic={"会社概要":"COMPANY",
+     "事業内容":"SERVICE",
+     "採用情報":"CAREER",
+     "コマンド":"COMMAND",
+     "おはよう":"5",
+     "こんにちは":"5",
+     "こんばんは":"5",
+     "こんにちわ":"6",
+     "こんばんわ":"6"
+     }
+count=0
 
 def getResponse(self,text):
-    
+    global count
+    if count>=4:
+        count=0
+    count+=1
     for _dic in dic:
         if _dic==text:
             return dic[text]
-    
-    return random.randint(1,3)
+    return count
 
