43c7f40cdc102177c11adb2a17e50b8450e69445
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-02-25 16:06:44 +0900

    Add files via upload

--- line_bot/main.py
+++ line_bot/main.py
@@ -35,12 +35,14 @@ handler = WebhookHandler('LINE_CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
-
+    
+    # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-
+    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -48,6 +50,9 @@ def callback():
 
     return 'OK'
 
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
