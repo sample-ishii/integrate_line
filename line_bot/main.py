6f790296d27e3fcdaa568c2a11442bb1cd9d7102
ishii <42855864+connectcrew-ishii@users.noreply.github.com>
2019-02-26 14:39:57 +0900

    Update main.py
    58行目を更新

--- line_bot/main.py
+++ line_bot/main.py
@@ -55,7 +55,7 @@ def handle_message(event):
     #入力された内容(event.message.text)に応じて返信する
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=os.getenv(res.getResponse(event.message.text)))
+    TextSendMessage(text=event.message.text)
     )
 
 if __name__ == "__main__":
